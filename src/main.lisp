655a192309421b9d1fd2486e966a46f803b0c841
crowdigit <87285844+crowdigit@users.noreply.github.com>
2023-11-28 18:43:06 +0900

    refactor: use non-global foo

--- src/main.lisp
+++ src/main.lisp
@@ -8,14 +8,15 @@
 (defstruct player
   damage)
 
-(defparameter *foos* nil)
 (defparameter *foo-num* 5)
 (defparameter *foo-hp-max* 25)
 
 (defparameter *player* (make-player :damage '(6 . (1 . 3))))
 
-(loop for x to *foo-num*
-      do (push (make-foo :hp (1+ (random *foo-hp-max*))) *foos*))
+(defun init-foos (*random-state*)
+  (mapcar (lambda (hp) (make-foo :hp hp))
+          (loop for x to *foo-num*
+                collect (random *foo-hp-max*))))
 
 (defun foo-print (foo)
   (format t "Foo (HP: ~d)~c" (foo-hp foo) #\newline))
@@ -32,13 +33,6 @@
             (+ damage-a (dice-max damage-b))
             damage-a (dice-desc damage-b) #\newline)))
 
-(defun player-damage-det (player *random-state*)
-  (let ((damage-a (car (player-damage player)))
-        (damage-b (cdr (player-damage player))))
-    (+ damage-a (ndf (car damage-b) (cdr damage-b) *random-state*))))
-
-(player-damage-det *player* *random-state*)
-
 (defun 1df (f *random-state*)
   (1+ (random f *random-state*)))
 
@@ -47,5 +41,13 @@
           (loop for x from 1 to n
                 collect (1df f *random-state*))))
 
-(progn (mapc #'foo-print *foos*)
-       (player-damage-desc *player*))
+(defun player-damage-det (player *random-state*)
+  (let ((damage-a (car (player-damage player)))
+        (damage-b (cdr (player-damage player))))
+    (+ damage-a (ndf (car damage-b) (cdr damage-b) *random-state*))))
+
+(player-damage-det *player* *random-state*)
+
+(defun main ()
+  (let ((foos ())))
+  )
